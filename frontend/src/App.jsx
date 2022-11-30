789dfecd8de07db6cc9fe2c8fbefb66ef005465a
manuelJung <manuel.jung.wwi12@gmail.com>
2022-11-30 14:36:22 +0100

    setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import {Route, Routes} from 'react-router-dom'
+import Account from './routes/Account'
+import CreateQuestion from './routes/CreateQuestion'
+import Login from './routes/Login'
+import Overview from './routes/Overview'
+import QuestionById from './routes/QuestionById'
+import Register from './routes/Register'
+
 
 export default function App() {
   return (
     <div className="App">
-      App
+      <Routes>
+        <Route path='/' element={<Overview/>}/>
+        <Route path='/question/:id' element={<QuestionById/>}/>
+        <Route path='/create-question' element={<CreateQuestion/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/account' element={<Account/>}/>
+      </Routes>
     </div>
   )
 }
