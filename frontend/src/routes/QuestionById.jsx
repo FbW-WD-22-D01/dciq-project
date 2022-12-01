7e19a042eea18ba75532909b075f627afde929d0
manuelJung <manuel.jung.wwi12@gmail.com>
2022-12-01 15:15:55 +0100

    created questions page

--- frontend/src/routes/QuestionById.jsx
+++ frontend/src/routes/QuestionById.jsx
@@ -1,7 +1,25 @@
+import React from 'react'
 import {useParams} from 'react-router'
+import axios from 'axios'
+import './QuestionById.scss'
 
 export default function QuestionById () {
   const { id } = useParams()
+  const [question, setQuestion] = React.useState(null)
+
+  React.useEffect(() => {
+    axios.get('/questions/'+id).then(response => setQuestion(response.data))
+  }, [id])
+
+  if(!question) return null
   
-  return <h1>QuestionById: {id}</h1>
+  return (
+    <div className='QuestionById'>
+      <h1>{question.title}</h1>
+      <p>{question.description}</p>
+      <div className='user-name'>{question.user.name}</div>
+
+      <hr/>
+    </div>
+  )
 }
