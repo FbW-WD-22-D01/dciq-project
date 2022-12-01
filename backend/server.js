dc239937744fac3972c51225e0567e759b3b7b51
manuelJung <manuel.jung.wwi12@gmail.com>
2022-12-01 13:52:09 +0100

    connected frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   res.status(err.status || 500).send({
-    message: err.message
+    message: err.message // [{name: "wir brauchen einen Namen"}]
   })
 })
 
