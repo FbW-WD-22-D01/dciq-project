3f8fe00a30a6a389030488c50f21ef73db7f7766
manuelJung <manuel.jung.wwi12@gmail.com>
2022-12-01 15:47:33 +0100

    implemented answers endpoints

--- backend/src/controllers/answers.js
+++ backend/src/controllers/answers.js
@@ -1,13 +1,26 @@
 import httpErrors from 'http-errors'
+import Answer from '../models/Answer.js'
 
 /** @type {import("express").RequestHandler} */
-export function getAnswers () {
-  throw httpErrors.NotImplemented()
+export async function getAnswers (req, res) {
+  let query = Answer.find()
+
+  query = query.populate('user', 'name -_id')
+
+  if(req.query.questionId) query = query.where('question').equals(req.query.questionId)
+
+  const answers = await query
+
+  res.status(200).send(answers)
 }
 
 /** @type {import("express").RequestHandler} */
-export function createAnswer () {
-  throw httpErrors.NotImplemented()
+export async function createAnswer (req, res) {
+  const answer = new Answer(req.body)
+  answer.user = req.user._id
+  await answer.save()
+
+  res.status(201).send(answer)
 }
 
 /** @type {import("express").RequestHandler} */
--- backend/src/routers/answers.js
+++ backend/src/routers/answers.js
@@ -2,11 +2,12 @@ import express from 'express'
 import 'express-async-errors'
 import * as controller from '../controllers/answers.js'
 import * as validation from '../lib/validators/answers.js'
+import auth from '../lib/middlewares/auth.js'
 
 const app = express.Router()
 
 app.get('/', controller.getAnswers)
-app.post('/', validation.createAnswer, controller.createAnswer)
+app.post('/', auth, validation.createAnswer, controller.createAnswer)
 app.get('/created-by-me', controller.getAnswersCreatedByMe)
 
 export default app
