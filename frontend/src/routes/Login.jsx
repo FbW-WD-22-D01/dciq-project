d23fa0ac610a753a9e44f16223d5a077cbac1c03
manuelJung <manuel.jung.wwi12@gmail.com>
2022-12-01 11:24:39 +0100

    created Login & Register UI

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,5 +1,27 @@
-
+import {Link} from 'react-router-dom'
+import './Login.scss'
 
 export default function Login () {
-  return <h1>Login</h1>
+
+  return (
+    <form className='Login'>
+      <h2>Login</h2>
+      
+      <div className='input-row'>
+        <label htmlFor='email'>Email</label>
+        <input id='email' type='email' placeholder='Email...'/>
+      </div>
+      <div className='input-row'>
+        <label htmlFor='password'>Password</label>
+        <input id='password' type='password' placeholder='Password...'/>
+      </div>
+
+      <Link to='/register' className='link'>
+        Ich habe noch keinen Account
+      </Link>
+
+      <button>Login</button>
+      <span className='error'>An Error happened</span>
+    </form>
+  )
 }
--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -1,5 +1,30 @@
-
+import {Link} from 'react-router-dom'
+import './Register.scss'
 
 export default function Register () {
-  return <h1>Register</h1>
+  return (
+    <form className='Register'>
+      <h2>Registrieren</h2>
+      
+      <div className='input-row'>
+        <label htmlFor='name'>Name</label>
+        <input id='name' type='text' placeholder='Name...'/>
+      </div>
+      <div className='input-row'>
+        <label htmlFor='email'>Email</label>
+        <input id='email' type='email' placeholder='Email...'/>
+      </div>
+      <div className='input-row'>
+        <label htmlFor='password'>Password</label>
+        <input id='password' type='password' placeholder='Password...'/>
+      </div>
+
+      <Link to='/login' className='link'>
+        Ich habe bereits einen Account
+      </Link>
+
+      <button>Registrieren</button>
+      <span className='error'>An Error happened</span>
+    </form>
+  )
 }
